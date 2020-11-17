03a2b2e0da89543cbc30aa7a155c39ce24d42bf7
pricesco <62159911+pricesco@users.noreply.github.com>
2020-11-17 06:57:37 -0500

    "Added URL and View form Screen 4"

--- BizWiz-Project/BizWiz/BizWiz/urls.py
+++ BizWiz-Project/BizWiz/BizWiz/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('accounts/signup/', main.SignUpView.as_view(), name='signup'),
     path('accounts/signup/business/', business.BusinessSignUpView.as_view(), name='business_signup'),
     path('accounts/signup/individual/', individual.IndividualSignUpView.as_view(), name='individual_signup'),
+    path('view_business/<str:business_name>', business.display_business, name='display_business'),
 ]
--- BizWiz-Project/BizWiz/main/views/business.py
+++ BizWiz-Project/BizWiz/main/views/business.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import login
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.views.generic import CreateView
 
 from ..forms import BusinessSignUpForm
@@ -18,4 +18,13 @@ class BusinessSignUpView(CreateView):
         user = form.save()
         login(self.request, user)
         print("Success") 
+        return redirect('home')
+
+
+def display_business(request, business_name):
+    business = UserProfile.objects.get(business_name=business_name)
+    if business.is_Business:
+        context = {'business': business}
+        return render(request, 'main/business_display.html')
+    else:
         return redirect('home')
