0e9c37af4a71476edb6c79602eccd0354dbe4be3
pricesco <62159911+pricesco@users.noreply.github.com>
2020-11-17 07:26:39 -0500

    Changed type

--- BizWiz-Project/BizWiz/main/views/business.py
+++ BizWiz-Project/BizWiz/main/views/business.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect, render
 from django.views.generic import CreateView
 
 from ..forms import BusinessSignUpForm
-from ..models import UserProfile
+from ..models import UserProfile, Business
 
 class BusinessSignUpView(CreateView):
     model = UserProfile
@@ -22,7 +22,7 @@ class BusinessSignUpView(CreateView):
 
 
 def display_business(request, business_name):
-    business = UserProfile.objects.get(business_name=business_name)
+    business = Business.objects.get(business_name=business_name)
     if business.is_Business:
         context = {'business': business}
         return render(request, 'main/business_display.html')
