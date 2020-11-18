0dc6043cc6ec317e8b2ac818ce1f67d5c0dbfa42
brady <bradyhuai@outlook.com>
2020-11-18 02:14:41 -0800

    Add post job page textfield

--- BizWiz-Project/bizwizfrontend/src/components/PostjobPage.jsx
+++ BizWiz-Project/bizwizfrontend/src/components/PostjobPage.jsx
@@ -5,49 +5,64 @@ import { makeStyles } from "@material-ui/core/styles";
 
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      marginTop: 90,
-      marginLeft: 220,
-      marginBottom: 10,
-      height: "100%",
-      textAlign: "left",
-    },
+        root: {
+        marginTop: 90,
+        marginLeft: 220,
+        marginBottom: 10,
+        height: "100%",
+        textAlign: "left",
+        },
+        form: {
+            margin: "20px",
+            height: "100%",
+            width: "80%",
+        },
+        boxText: {
+            marginTop: "20px"
+        }
     })
 );
 
 
 export default function Postjob() {
     const classes = useStyles();
+
     return (
-        <form autoComplete="off" className={classes.root}>
-          <Typography variant="h6">
-        Basic Information
-      </Typography>
-          <Grid container spacing={3}>
-            <Grid item md={6} sm={12} xs={12}>
-              <TextField label="First Name" variant="outlined" fullWidth required />
-            </Grid>
-            <Grid item md={6} sm={12} xs={12}>
-              <TextField label="Last Name" variant="outlined" fullWidth required />
-            </Grid>
-          </Grid>
-          <Typography variant="h6">
-        More about this job
-      </Typography>
-          <Grid container spacing={3}>
-            <Grid item md={6} sm={12} xs={12}>
-              <TextField label="City" variant="outlined" fullWidth />
-            </Grid>
-            <Grid item md={6} sm={12} xs={12}>
-              <TextField label="Region" variant="outlined" fullWidth />
-            </Grid>
-          </Grid>
-          <TextField
-            variant="outlined"
-            label="Occupation"
-            fullWidth
-          />
-          <Divider />
-        </form>
+        <div className={classes.root}>
+            <form autoComplete="off" className={classes.form}>
+                <Typography variant="h6">
+                    Basic Information
+                </Typography>
+                <Grid container spacing={3}>
+                    <Grid item md={6} sm={12} xs={12}>
+                        <TextField label="Position type" variant="outlined" fullWidth required />
+                    </Grid>
+                    <Grid item md={6} sm={12} xs={12}>
+                        <TextField label="Job Title" variant="outlined" fullWidth required />
+                    </Grid>
+                    <Grid item md={6} sm={12} xs={12}>
+                        <TextField label="Location" variant="outlined" fullWidth required />
+                    </Grid>
+                    <Grid item md={6} sm={12} xs={12}>
+                        <TextField label="Salary" variant="outlined" fullWidth required />
+                    </Grid>
+                </Grid>
+                <br />
+                <Typography variant="h6">
+                    More about this job
+                </Typography>
+                <Grid container spacing={3}>
+                    <Grid item md={6} sm={12} xs={12}>
+                        <TextField label="Deadline" variant="outlined" fullWidth required />
+                    </Grid>
+                    <Grid item md={6} sm={12} xs={12}>
+                        <TextField label="Application link" variant="outlined" fullWidth required />
+                    </Grid>
+                </Grid>
+                <TextField className={classes.boxText} label="Detailed description about this job" variant="outlined" fullWidth multiline rows={4}/>
+                <TextField className={classes.boxText} label="Job requirements" variant="outlined" fullWidth multiline rows={4}/>
+                <TextField className={classes.boxText} label="Additional notes" variant="outlined" fullWidth multiline rows={4}/>
+            </form>
+        </div>
     );
 }
