3ac8aba112a8da43da63ba679402d194fe051145
michael <skyjoy.liu@mail.utoronto.ca>
2020-11-17 22:08:00 -0800

    Give sidebar onclick

--- BizWiz-Project/bizwizfrontend/src/features/HomePage/sidebar.jsx
+++ BizWiz-Project/bizwizfrontend/src/features/HomePage/sidebar.jsx
@@ -6,32 +6,46 @@ import DashboardIcon from "@material-ui/icons/Dashboard";
 import PeopleIcon from "@material-ui/icons/People";
 import BarChartIcon from "@material-ui/icons/BarChart";
 import SearchIcon from "@material-ui/icons/Search";
+import { useHistory } from "react-router";
 
-export const mainListItems = (
-  <div>
-    <ListItem button>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Dashboard" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <SearchIcon />
-      </ListItemIcon>
-      <ListItemText primary="Search Jobs" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Profile" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Reports" />
-    </ListItem>
-  </div>
-);
+export default function MainListItems() {
+  const history = useHistory();
+  const handleClickProfile = () => {
+    history.push("/pages/profilepage");
+  };
+  const handleClickDashboard = () => {
+    history.push("/pages/home");
+  };
+
+  const handleClickSearch = () => {
+    history.push("/pages/search");
+  };
+  return (
+    <div>
+      <ListItem button onClick={handleClickDashboard}>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="Dashboard" />
+      </ListItem>
+      <ListItem button onClick={handleClickSearch}>
+        <ListItemIcon>
+          <SearchIcon />
+        </ListItemIcon>
+        <ListItemText primary="Search Jobs" />
+      </ListItem>
+      <ListItem button onClick={handleClickProfile}>
+        <ListItemIcon>
+          <PeopleIcon />
+        </ListItemIcon>
+        <ListItemText primary="Profile" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <BarChartIcon />
+        </ListItemIcon>
+        <ListItemText primary="Reports" />
+      </ListItem>
+    </div>
+  );
+}
