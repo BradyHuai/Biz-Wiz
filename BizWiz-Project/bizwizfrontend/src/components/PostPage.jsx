67b6a22783acb2b4715df3b28ab9e3c36d5f45b2
brady <bradyhuai@outlook.com>
2020-11-17 23:40:28 -0800

    Change table style

--- BizWiz-Project/bizwizfrontend/src/components/PostPage.jsx
+++ BizWiz-Project/bizwizfrontend/src/components/PostPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { Typography, Paper } from "@material-ui/core";
+import { Typography, Paper, Collapse } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -11,12 +11,38 @@ const useStyles = makeStyles((theme) => ({
     height: "100%",
     textAlign: "left",
   },
-  paper: {
-    margin: 10,
+  table: {
+    borderRadius: "0px 0px 0px 0px",
+    width: "100%",
+    display: "table",
+    border: "1px solid",
+    borderCollapse: "separate",
+    lineHeight: "15px",
   },
-  postingtitle: {
+  tableHead: {
+    display: "table-header-group",
+    verticalAlign: "middle",
+    backgroundColor: "#D9EDF7",
+  },
+  title: {
+    borderColor: "#7e7e7e #7e7e7e #7e7e7e #7e7e7e",
+  },
+  tableBody: {
+    display: "table-row-group",
+    verticalAlign: "middle",
+    borderTop: "1px solid",
+  },
+  row: {
     textAlign: "left",
-    padding: 20,
+    verticalAlign: "top",
+    display: "table-row",
+  },
+  cell: {
+      display: "table-cell",
+      padding: "4px",
+      lineHeight: "18px",
+      borderTop: "1px solid #ddd",
+      borderLeft: "1px solid #ddd",
   },
 }));
 
@@ -26,38 +52,84 @@ export default function PostPage() {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <Paper className={classes.paper}>
-        <Paper variant="outlined">
-          <img src="/images/bwlogo.png" style={{ margin: 10 }}></img>
-        </Paper>
-
-        <Paper variant="outlined">
-          <Typography variant="body1" className={classes.postingtitle}>
-            Website: biz-wiz.ca
-          </Typography>
-        </Paper>
-        <Paper variant="outlined">
-          <Typography variant="body1" className={classes.postingtitle}>
-            Email: bizwiz@bizwiz.ca
-          </Typography>
-        </Paper>
-        <Paper variant="outlined">
-          <Typography variant="body1" className={classes.postingtitle}>
-            Address: 123 Bizwiz Street
-          </Typography>
-        </Paper>
-        <Paper variant="outlined">
-          <Typography variant="body1" className={classes.postingtitle}>
-            Contact: Michelle
-          </Typography>
-        </Paper>
-      </Paper>
-
-      <Paper className={classes.paper}>
-        <Typography variant="h6" className={classes.postingtitle}>
-            JOB POSTING INFORMATION
-        </Typography>
-      </Paper>
+      <table className = {classes.table}>
+          <thead className={classes.tableHead}>
+              <tr className={classes.title}>
+                  <th>JOB POSTING INFORMATION</th>
+              </tr>
+          </thead>
+          <tbody className={classes.tableBody}>
+              <tr className={classes.row}>
+                  <td className={classes.cell} width="25%">Position type:</td>
+                  <td className={classes.cell} width="75%">Full-time</td>
+              </tr>
+              <tr className={classes.row}>
+                  <td className={classes.cell} width="25%">Job Title:</td>
+                  <td className={classes.cell} width="75%">Full Stact Developer</td>
+              </tr>
+              <tr className={classes.row}>
+                  <td className={classes.cell} width="25%">Job Location:</td>
+                  <td className={classes.cell} width="75%">123123 Street Toronto</td>
+              </tr>
+              <tr className={classes.row}>
+                  <td className={classes.cell} width="25%">Salary:</td>
+                  <td className={classes.cell} width="75%">$50k annually</td>
+              </tr>
+              <tr className={classes.row}>
+                  <td className={classes.cell} width="25%">About company:</td>
+                  <td className={classes.cell} width="75%">
+                      <p>
+                        Our company is a deep technology start-up dedicated to developing 
+                        advanced sensing and control solutions for electrochemical system 
+                        optimization. Our proprietary hardware and software are first-in-class, 
+                        enabling users of electrochemical systems to gain previously inaccessible 
+                        insights into their operations. 
+                      </p>
+                  </td>
+              </tr>
+              <tr className={classes.row}>
+                  <td className={classes.cell} width="25%">Job description:</td>
+                  <td className={classes.cell} width="75%">
+                      <p>
+                        We are looking for a skilled, curious, and driven full stack developer to lead the development 
+                        of our front-end web architecture and to ensure the responsiveness of our applications as we 
+                        grow and bring our technologies to market. 
+                      </p>
+                  </td>
+              </tr>
+              <tr className={classes.row}>
+                  <td className={classes.cell} width="25%">Job Requirements:</td>
+                  <td className={classes.cell} width="75%">
+                      <strong>Requirements:</strong>
+                      <ul>
+                          <li>A B.S./B.A., M.Sc., or PhD in computer science, math, engineering, or related field (min GPA 3.5/4)</li>
+                          <li> Proficiency in one or more general purpose programming languages including (but not limited to) Java, C/C++, C#, Objective C, Python, JavaScript, or Go</li>
+                          <li> Familiarity with SQL and NoSQL databases</li>
+                      </ul>
+                      <strong>Bonus:</strong>
+                      <ul>
+                          <li>Knowledge and understanding of embedded systems programming and/or systems modelling</li>
+                          <li>Experience developing and deploying statistical models and AI pipelines</li>
+                          <li>Knowledge and understanding of control theory </li>
+                      </ul>
+                  </td>
+              </tr>
+          </tbody>
+      </table>
+      <table className = {classes.table}>
+            <thead className={classes.tableHead}>
+              <tr className={classes.title}>
+                    <th>APPLICATION INFORMATION</th>
+              </tr>
+            </thead>
+      </table>
+      <table className = {classes.table}>
+            <thead className={classes.tableHead}>
+              <tr className={classes.title}>
+                    <th>ORGANIZATION INFORMATION</th>
+              </tr>
+            </thead>
+      </table>
     </div>
   );
 }
