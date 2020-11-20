52c4b3dd8137ff76f5e55a60fe7d8a1c858620f0
Artur Kuramshin <artur.kurams@gmail.com>
2020-11-19 23:04:58 -0500

    Added locations to admin page

--- BizWiz-Project/BizWiz/main/admin.py
+++ BizWiz-Project/BizWiz/main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Business, Individual, UserProfile, Industry
+from .models import Business, Individual, UserProfile, Industry, Location
 
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Business)
 admin.site.register(Individual)
 admin.site.register(Industry)
+admin.site.register(Location)
