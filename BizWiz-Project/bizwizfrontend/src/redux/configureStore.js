54f532f78c3417b4c5e4c653490685ff857c9da2
michael <skyjoy.liu@mail.utoronto.ca>
2020-11-29 12:04:15 -0800

    Add redux

---
+++ BizWiz-Project/bizwizfrontend/src/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { combineReducers, createStore, applyMiddleware } from "redux";
+// import axiosMiddleware from "redux-axios-middleware";
+// import axios from "axios";
+import userinfoReducer from "./ducks/userinfo";
+
+const reducer = combineReducers({
+  userinfo: userinfoReducer,
+});
+
+const store = createStore(reducer);
+
+export default store;
