3ed184d6c12fbb539d1916286b874581ac5b324c
pricesco <62159911+pricesco@users.noreply.github.com>
2020-12-02 20:40:27 -0500

    changed the way keywords are stored

--- BizWiz-Project/BizWiz/main/views/api.py
+++ BizWiz-Project/BizWiz/main/views/api.py
@@ -4,6 +4,7 @@ from knox.models import AuthToken
 from rest_framework.views import APIView
 from ..serializers import *
 from ..industries import Industries
+from ..keywords import Keywords
 from ..models import Location, Post, Business, Application, UserProfile, Individual
 import requests
 
@@ -66,7 +67,7 @@ class OptionsView(APIView):
         return Response({
             'cities' : list({location.city for location in Location.objects.all()}),
             'industry' : list({industry[1] for industry in Industries.get()}),
-            "keyword" : ["artist", "chef", "software engineer"]
+            "keyword" : Keywords.get()
         })
 
 
